077ec53b209e2cd92a3096e48cf5c6917cdff8f5
K4-R4 <mabdman512@gmail.com>
2023-08-20 17:38:16 +0900

    edit makefile

--- Makefile
+++ Makefile
@@ -1,27 +1,34 @@
 CFLAGS  = -g -MMD -MP -Wall -Wextra -Werror
 LDFLAGS =
-LIBS    =
+LIBS    = libft/libft.a
 INCLUDE = -I./include -I./libft/include
 NAME    = minishell
-SRCDIR  = ./src
+SRCDIR  = src
 SRCS    = main.c
-OBJDIR  = ./obj
+OBJDIR  = obj
 OBJS    = $(addprefix $(OBJDIR)/, $(notdir $(SRCS:.c=.o)))
 DEPENDS = $(OBJS:.o=.d)
 
+all: $(NAME)
+
 $(NAME): $(OBJS) $(LIBS)
-	$(CC) -o $@ $^ $(LDFLAGS)
+	$(CC) -o $@ $^ $(LDFLAGS) $(LIBS)
 
 $(OBJDIR)/%.o: $(SRCDIR)/%.c
-	-mkdir -p $(OBJDIR)
+	@ mkdir -p $(OBJDIR)
 	$(CC) $(CFLAGS) $(INCLUDE) -o $@ -c $<
 
-all: fclean $(NAME)
+$(LIBS):
+	$(MAKE) -C libft
 
 clean:
-	-$(RM) -r $(OBJDIR)
+	make -C libft clean
+	$(RM) -r $(OBJDIR)
 
 fclean: clean
-	-$(RM) $(NAME)
+	make -C libft fclean
+	$(RM) $(NAME)
+
+re: fclean all
 
 -include $(DEPENDS)
--- src/builtin/ms_echo.c
+++ src/builtin/ms_echo.c
@@ -6,7 +6,7 @@
 /*   By: tkuramot <tkuramot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/20 10:35:18 by tkuramot          #+#    #+#             */
-/*   Updated: 2023/08/20 10:35:45 by tkuramot         ###   ########.fr       */
+/*   Updated: 2023/08/20 17:18:02 by tkuramot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,12 @@
 
 int	main(int argc, char **argv)
 {
+	if (argc == 1)
+	{
+		ft_putstr_fd("\n", STDOUT_FILENO);
+		return (EXIT_SUCCESS);
+	}
+	if (ft_strncmp(argv[1], "-n", 2) == 0)
+		;
 	return (EXIT_SUCCESS);
 }
