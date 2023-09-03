ef0586757b6cb00b3ba409a97f928ab55c93c947
K4-R4 <mabdman512@gmail.com>
2023-09-03 12:22:03 +0900

    Add struct

--- include/parser.h
+++ include/parser.h
@@ -6,7 +6,7 @@
 /*   By: tkuramot <tkuramot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/24 12:19:42 by tkuramot          #+#    #+#             */
-/*   Updated: 2023/08/24 12:20:08 by tkuramot         ###   ########.fr       */
+/*   Updated: 2023/09/03 12:21:05 by tkuramot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,21 @@
 # define PARSER_H
 
 # include "libft.h"
+# include "lexer.h"
+
+typedef enum e_node_type
+{
+	ND_CMD,
+	ND_PIPE
+} t_node_type;
+
+typedef struct s_ast t_ast;
+struct s_ast
+{
+	t_node_type	type;
+	t_ast		*left;
+	t_ast		*right;
+	t_token		*lst;
+};
 
 #endif
