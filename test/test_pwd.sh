f2e3dab71087eb550968792dbcb17d986c65b4b2
K4R4 <mabdman512@gmail.com>
2023-09-03 19:48:04 +0900

    Modify test

--- test/test_pwd.sh
+++ test/test_pwd.sh
@@ -3,12 +3,12 @@
 func=$(find . -name "assert.sh")
 source $func
 
-assert_pwd ''
-assert_pwd 'aaaaaaa'
-assert_pwd 'aaa bbb ccc'
-assert_pwd '  aaa   bbb    ccc '
-#assert_pwd '-nnnnan'
-#assert_pwd '-a'
-#assert_pwd '---------n'
-assert_pwd '--'
-assert_pwd '-'
+assert
+assert aaaaaaa
+assert aaa bbb ccc
+assert   aaa   bbb    ccc
+#asserd -nnnnan
+#asserd -a
+#asserd ---------n
+assert --
+assert -
