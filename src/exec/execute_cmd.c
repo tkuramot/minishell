ab7b6e311393bd380a17e93802eec431fe42b3e4
K4R4 <mabdman512@gmail.com>
2023-09-14 00:53:42 +0900

    multiple pipe works

--- src/exec/execute_cmd.c
+++ src/exec/execute_cmd.c
@@ -6,7 +6,7 @@
 /*   By: tkuramot <tkuramot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/10 17:03:24 by tkuramot          #+#    #+#             */
-/*   Updated: 2023/09/13 23:10:22 by tkuramot         ###   ########.fr       */
+/*   Updated: 2023/09/14 00:50:30 by tkuramot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,43 @@
 void	execute_pipe(t_ast *ast, t_env *env_lst)
 {
 	int	pipe_fd[2];
+	int	pid;
 
-	if (pipe(pipe_fd) == -1)
-		fatal_error("pipe");
+	if (!ast)
+		return;
+	if (ast->type == ND_PIPE)
+	{
+		if (pipe(pipe_fd) == -1)
+			fatal_error("pipe");
+		pid = fork();
+		if (pid == 0)
+		{
+			close(pipe_fd[PIPE_READ]);
+			dup2(pipe_fd[PIPE_WRITE], STDOUT_FILENO);
+			execute_pipe(ast->left, env_lst);
+		}
+		else
+		{
+			close(pipe_fd[PIPE_WRITE]);
+			dup2(pipe_fd[PIPE_READ], STDIN_FILENO);
+			close(pipe_fd[PIPE_READ]);
+			char	**command;
+			char	**env;
+
+			command = token_lst_to_array(ast->right->lst);
+			env = env_list_to_array(env_lst);
+			handle_command(command, env);
+		}
+	}
+	else if (ast->type == ND_CMD)
+	{
+		char	**command;
+		char	**env;
+
+		command = token_lst_to_array(ast->lst);
+		env = env_list_to_array(env_lst);
+		handle_command(command, env);
+	}
 }
 
 void	execute(t_ast *ast, t_env *env_lst)
