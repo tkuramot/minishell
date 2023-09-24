1b8032a290976d039d136aa4725ecea57735aca0
K4-R4 <mabdman512@gmail.com>
2023-09-24 15:55:34 +0900

    wip

--- src/exec/execute.c
+++ src/exec/execute.c
@@ -6,7 +6,7 @@
 /*   By: tkuramot <tkuramot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/10 17:03:24 by tkuramot          #+#    #+#             */
-/*   Updated: 2023/09/24 13:37:06 by tkuramot         ###   ########.fr       */
+/*   Updated: 2023/09/24 15:54:29 by tkuramot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,46 +14,34 @@
 #include "parser.h"
 #include "utils.h"
 
-void	traverse_pipe(t_ast *ast, t_env *env_lst, t_list **proc_lst)
+void	traverse_pipe(int std[2], t_ast *ast, t_env *env_lst, t_list **proc_lst)
 {
 	pid_t	pid;
-	int	pipe_fd[2];
-	int	out_fd;
+	int		pp[2];
+	int		tmp;
 
 	if (!ast)
 		return;
 	if (ast->type == ND_PIPE)
 	{
-		if (pipe(pipe_fd) == -1)
+		if (pipe(pp) == -1)
 			fatal_error("pipe");
-		pid = fork();
-		if (pid == 0)
-		{
-			close(pipe_fd[PIPE_WRITE]);
-			dup2(pipe_fd[PIPE_READ], STDIN_FILENO);
-			close(pipe_fd[PIPE_READ]);
-			run_cmd_parent(ast->right->lst, env_lst);
-		}
-		else
-		{
-			dprintf(STDERR_FILENO, "pid for cmd [%s] [%d]\n", ast->right->lst->word, pid);
-			ft_lstadd_front(proc_lst, ft_lstnew(ft_itoa(pid)));
-			out_fd = dup(STDOUT_FILENO);
-			close(pipe_fd[PIPE_READ]);
-			dup2(pipe_fd[PIPE_WRITE], STDOUT_FILENO);
-			close(pipe_fd[PIPE_WRITE]);
-			traverse_pipe(ast->left, env_lst, proc_lst);
-			dup2(out_fd, STDOUT_FILENO);
-		}
+		tmp = std[0];
+		std[0] = pp[0];
+		traverse_pipe(std, ast->right, env_lst, proc_lst);
+		close(pp[0]);
+		std[0] = tmp;
+		std[1] = pp[1];
+		traverse_pipe(std, ast->left, env_lst, proc_lst);
+		close(pp[1]);
 	}
 	else if (ast->type == ND_CMD)
 	{
-		close(pipe_fd[PIPE_READ]);
-		close(pipe_fd[PIPE_WRITE]);
 		pid = fork();
 		if (pid == 0)
+		{
 			run_cmd_parent(ast->lst, env_lst);
-		dprintf(STDERR_FILENO, "pid for cmd [%s] %d\n", ast->lst->word, pid);
+		}
 		ft_lstadd_front(proc_lst, ft_lstnew(ft_itoa(pid)));
 	}
 }
@@ -80,13 +68,16 @@ int	execute(t_ast *ast, t_env *env_lst)
 {
 	t_list *proc_lst;
 	int		status;
+	int		std[2];
 
 	proc_lst = NULL;
 	if (!ast)
 		return (1);
 	if (ast->type == ND_PIPE)
 	{
-		traverse_pipe(ast, env_lst, &proc_lst);
+		std[0] = 0;
+		std[1] = 1;
+		traverse_pipe(std, ast, env_lst, &proc_lst);
 		status = wait_all_children(proc_lst);
 		return (status);
 	}
