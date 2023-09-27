1e0dc0453321cb7a76b26819124792868c0931cf
K4-R4 <mabdman512@gmail.com>
2023-09-27 15:46:05 +0900

    handle sigint

--- src/utils/signal.c
+++ src/utils/signal.c
@@ -6,7 +6,7 @@
 /*   By: tsishika <tsishika@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 21:11:22 by tkuramot          #+#    #+#             */
-/*   Updated: 2023/09/27 14:44:08 by tkuramot         ###   ########.fr       */
+/*   Updated: 2023/09/27 15:45:46 by tkuramot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,24 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
+static void	sigint_handler(int sig)
+{
+	if (sig != SIGINT)
+		return;
+	ft_dprintf(STDOUT_FILENO, "\n");
+	rl_replace_line("", 0);
+	rl_on_new_line();
+	rl_redisplay();
+}
+
 void	set_signal_handler(void)
 {
 	struct sigaction	sa;
 
 	ft_bzero(&sa, sizeof(struct sigaction));
 	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = SA_SIGINFO;
 	sa.sa_handler = SIG_IGN;
 	sigaction(SIGQUIT, &sa, NULL);
+	sa.sa_handler = sigint_handler;
+	sigaction(SIGINT, &sa, NULL);
 }
